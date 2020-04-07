fc912b972388cce1158dbf9d4a7a419bee46d2c9
Anil Poudyal <anilrunjakote@gmail.com>
2020-04-07 13:10:16 +0545

    Some Changes in Login UI

--- app/src/main/java/com/example/nepshirts_android/MainActivity.java
+++ app/src/main/java/com/example/nepshirts_android/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  String email,  password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
     }
+
+
 }
